6721fb00daaef124743ce657eec8448cc9b370af
8-2018 <glavonjic1504@gmail.com>
2020-11-09 23:44:59 +0100

    Preuzimanje podataka iz tabele ExerciseResults i smestanje u listBox

--- Exercises_8-2018/Exercises_8-2018/Form1.Designer.cs
+++ Exercises_8-2018/Exercises_8-2018/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxExerciseResults = new System.Windows.Forms.ListBox();
+            this.labelExerciseResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxExerciseResults
+            // 
+            this.listBoxExerciseResults.FormattingEnabled = true;
+            this.listBoxExerciseResults.ItemHeight = 20;
+            this.listBoxExerciseResults.Location = new System.Drawing.Point(208, 40);
+            this.listBoxExerciseResults.Name = "listBoxExerciseResults";
+            this.listBoxExerciseResults.Size = new System.Drawing.Size(555, 364);
+            this.listBoxExerciseResults.TabIndex = 0;
+            // 
+            // labelExerciseResult
+            // 
+            this.labelExerciseResult.AutoSize = true;
+            this.labelExerciseResult.Location = new System.Drawing.Point(91, 40);
+            this.labelExerciseResult.Name = "labelExerciseResult";
+            this.labelExerciseResult.Size = new System.Drawing.Size(111, 20);
+            this.labelExerciseResult.TabIndex = 1;
+            this.labelExerciseResult.Text = "Exercise results:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.labelExerciseResult);
+            this.Controls.Add(this.listBoxExerciseResults);
+            this.Name = "Form1";
+            this.Text = "Exercise Results";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxExerciseResults;
+        private System.Windows.Forms.Label labelExerciseResult;
     }
 }
 
--- Exercises_8-2018/Exercises_8-2018/Form1.cs
+++ Exercises_8-2018/Exercises_8-2018/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,41 @@ namespace Exercises_8_2018
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using(SqlConnection DConnection = new SqlConnection())
+            {
+                List<ExerciseResult> listEX = new List<ExerciseResult>();
+
+                string connString = "Integrated Security=true;Initial Catalog=FacultyDB;Data Source = (localdb)\\MSSQLLocalDB";
+
+                DConnection.ConnectionString = connString;
+                DConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = DConnection;
+                sqlCommand.CommandText = "SELECT * FROM ExerciseResults";
+
+                SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+                while(dataReader.Read())
+                {
+                    ExerciseResult exResult = new ExerciseResult();
+
+                    exResult.Id = dataReader.GetInt32(0);
+                    exResult.StudentName = dataReader.GetString(1);
+                    exResult.StudentIndex = dataReader.GetString(2);
+                    exResult.Points = dataReader.GetInt32(3);
+
+                    listEX.Add(exResult);
+                }
+
+                foreach(ExerciseResult z in listEX)
+                {
+                    listBoxExerciseResults.Items.Add(z);
+                }
+
+            }
+        }
     }
 }
